d16e40048dcaf52a56f0210fd77b3e72c59065e3
Saint-Jean Germila <germila@noukod.com>
2019-04-17 15:03:25 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,7 +27,7 @@ class ProductsController < ApplicationController
     respond_to do |format|
      format.html {render :show}
      format.json {render json: @product, status: 200}
-   end
+   end 
    end
 
   private
